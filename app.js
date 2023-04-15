9218ccccd0fe530256ba6c975bcd33d11caa6c02
Hercan Yavuztürk <hercanyavuzturk@gmail.com>
2023-04-15 23:35:08 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const fn = function () {
-  console.log('dfdgdggh');
-};
+const express = require('express');
+const path = require("path");
+
+const app = express();
+
+//MIDDLEWARE
+//middleware request ve response arasında yapılır
+
+// const myLogger = (req,res,next) => {
+//     console.log("Middleware Log 1");
+//     next();
+// }
+
+// const myLogger2 = (req,res,next) => {
+//     console.log("Middleware Log 2 ");
+//     next();
+// }
+
+app.use(express.static("public"));
+// app.use(myLogger);
+// app.use(myLogger2); 
+
+app.get('/' , (req,res) => {
+   
+    res.sendFile(path.resolve(__dirname, "temp/index.html"))
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+});
